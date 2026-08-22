79c5cc7602bdab3be5e9aa01b4315048efce47c7
Umesh Bogati <bogatiu17@gmail.com>
2026-08-22 15:25:02 +0545

    Updated.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import AppRoutes from "./routes/AppRoutes";
 import { useSocketConnection } from "./socket/useSocket";
 import useAuthSync from "./hooks/useAuthSync";
 
+// App: Main application component that sets up socket connection and auth sync
 const App = () => {
   useSocketConnection();
   useAuthSync();
